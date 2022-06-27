4ff79d43ea26f7aa0c28d84d3d604cb71da4ddff
Sergey <xo4ywka@yandex.ru>
2022-06-27 10:18:27 +0300

    example 010 поиск элемента в  массиве

---
+++ Example010/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 43, 65, 7, 877, 32, 5, 98, 3, 32 };
+int n = array.Length;
+int find = 32;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
